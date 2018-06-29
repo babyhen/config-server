f6c7a8d99e65c191aed4b7b33cd0607460221087
liujixin <liujixin@pawpaw.com>
2018-06-29 17:18:31 +0800

    111

---
+++ src/main/java/com/pawpaw/config_server/BootStrap.java
@@ -0,0 +1,15 @@
+package com.pawpaw.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class BootStrap {
+	public static void main(String[] args) {
+		SpringApplication.run(BootStrap.class, args);
+	}
+}
